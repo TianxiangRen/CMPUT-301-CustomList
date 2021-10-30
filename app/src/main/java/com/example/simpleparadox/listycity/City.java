ad3f488270009b8524403b6fe53c14c9c8bf0368
TianxiangRen <tianxia3@ualberta.ca>
2021-10-30 17:45:47 -0600

    added a test for hasCity

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -1,10 +1,10 @@
 package com.example.simpleparadox.listycity;
 
-public class City {
+public class City implements Comparable<City>{
     private String city;
     private String province;
 
-    City(String city, String province){
+    public City(String city, String province){
         this.city = city;
         this.province = province;
     }
@@ -16,4 +16,17 @@ public class City {
     String getProvinceName(){
         return this.province;
     }
+
+    /**
+     * This method compare the cities based on their city names.
+     * @param city
+     *      A candidate city that is going to be compared to the current city
+     * @return
+     *      -1, 0, 1 based on whether the city comes before or after or equal
+     */
+    @Override
+    public int compareTo(City city) {
+        return this.city.compareTo(city.getCityName());
+    }
+
 }
